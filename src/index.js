928d75ba61d88d3c20ecd532e068ee024434b8d2
mtokhiani <tokhiiani@gmail.com>
2023-01-22 21:01:55 +0400

    added css baseline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./Theme";
 import App from "./App";
 
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <CssBaseline enableColorScheme />
+        <App />
     </ThemeProvider>
   </React.StrictMode>
 );
