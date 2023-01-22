ce16b5df9b0eb170fb4a187e2036f625dd98bac1
mtokhiani <tokhiiani@gmail.com>
2023-01-22 21:30:21 +0400

    imported styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import "./styles/styles";
 import theme from "./Theme";
 import Header from "./components/Header";
 import App from "./App";
