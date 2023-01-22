2fcffadf01811e1321f47f70b7198ec2009f3fa7
mtokhiani <tokhiiani@gmail.com>
2023-01-22 21:29:23 +0400

    updated router placement

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./Theme";
+import Header from "./components/Header";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
-        <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
