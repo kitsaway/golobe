05232e499adae586e380f9e06681a688fbdb7154
mtokhiani <tokhiiani@gmail.com>
2023-01-23 21:01:43 +0400

    Moved logo to main component

--- src/components/AuthFlow/index.js
+++ src/components/AuthFlow/index.js
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { Grid } from "@mui/material";
 import ImageSwiper from "../ImageSwiper";
 
 const AuthFlow = ({ form }) => {
+  const navigate = useNavigate();
   return (
     <Grid
       container
@@ -17,6 +19,14 @@ const AuthFlow = ({ form }) => {
       }}
     >
       <Grid item xs={11} md={4}>
+        <img
+          src="https://ik.imagekit.io/xe8oxb5c5l/Golobe/Logo.png"
+          alt="Golobe logo"
+          width={110}
+          height={36}
+          className="login-logo"
+          onClick={() => navigate("/")}
+        />
         {form}
       </Grid>
       <Grid item md={6} bg={5} sx={{ display: { xs: "none", md: "flex" } }}>
