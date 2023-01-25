3015d16e3a20b94265e77200653c610a6c18c075
mtokhiani <tokhiiani@gmail.com>
2023-01-25 19:55:42 +0400

    Added Sign up page component

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,8 @@
+import SignupFlow from "../Flow/SignupFlow";
+import SignupForm from "../Forms/SignupForm";
+
+const SignUp = () => {
+  return <SignupFlow form={<SignupForm />} />;
+};
+
+export default SignUp;
