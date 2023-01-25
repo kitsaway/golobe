0675d0acaf9b55da095ac5198c5096279c95876e
mtokhiani <tokhiiani@gmail.com>
2023-01-25 21:26:14 +0400

    updated import

--- src/components/PaymentMethod/index.js
+++ src/components/PaymentMethod/index.js
@@ -1,5 +1,5 @@
 import SignupFlow from "../Flow/SignupFlow";
-import PaymentMethodForm from "./../Forms/PaymentMethod";
+import PaymentMethodForm from "./../Forms/PaymentMethodForm";
 
 const PaymentMethod = () => {
   return <SignupFlow form={<PaymentMethodForm />} />;
