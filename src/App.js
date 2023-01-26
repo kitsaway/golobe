75ed9f0dd307a94982be35dce5a82013abbcf00a
mtokhiani <tokhiiani@gmail.com>
2023-01-26 20:28:51 +0400

    Updated content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Container } from "@mui/material";
 import Header from "./components/Header";
+import Home from "./components/Home";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container maxWidth={false} className="container">
+        <Home />
+      </Container>
+    </>
+  );
 }
 
 export default App;
