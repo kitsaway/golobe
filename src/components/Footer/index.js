73652cca856768f055a98cef9e471460862272cb
mtokhiani <tokhiiani@gmail.com>
2023-01-30 00:16:23 +0400

    Created footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import Subscribe from "./Subscribe";
+
+const Footer = () => {
+  return <Box className="footer-container">
+    <Subscribe />
+  </Box>;
+};
+
+export default Footer;
