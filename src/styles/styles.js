0d8244a759ab21f1228b65671dd4edb7949a9220
mtokhiani <tokhiiani@gmail.com>
2023-01-30 00:16:49 +0400

    Style import update

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,5 +1,6 @@
 import "./index.css";
 import "./components/HeaderStyles.css";
+import "./components/FooterStyles.css";
 import "./components/FormStyles.css";
 import "./components/SwiperStyles.css";
 import "./components/HomeStyles.css";
