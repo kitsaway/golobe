38ff9f6580dbba6d37d420e20d99abb1c5d0ff79
mtokhiani <tokhiiani@gmail.com>
2023-01-30 00:17:00 +0400

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Container } from "@mui/material";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Container maxWidth={false} className="container">
         <Home />
       </Container>
+      <Footer />
     </>
   );
 }
